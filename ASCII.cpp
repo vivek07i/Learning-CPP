afa75324d8165def61c884ffc0267715a9f38817
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-08-10 12:00:17 -0700

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // This Code helps to Find the Ascii number for any symbol.
+    cout << "Hello World" << endl;
+
+    char c = '&';
+    cout << "The ASCII value of " << c << " is " << int(c);
+    return 0;
+
+    return 0;
+}
