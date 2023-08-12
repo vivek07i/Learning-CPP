d859eaa8d0996fa47f4e2ecd083d00714afc2817
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-08-12 10:51:51 -0700

    Add files via upload

---
+++ maths.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << max(5, 10) << endl;
+    cout << min(10,20) << endl;
+    cout << sqrt(64) << endl;
+    cout << round(2.6) << endl;
+    cout << log(2) << endl;
+    cout << cbrt(125) << endl;
+
+
+    return 0;
+}
