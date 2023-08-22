3b57bd31e6ddebbb1f3748052e804fe486a23ed1
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-08-22 10:44:18 -0700

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b , temp;
+    cin >> a >> b;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
