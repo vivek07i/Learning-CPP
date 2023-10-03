89f7a54438c3dc5a0c5590452817752d138f3e0a
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-10-03 21:16:36 +0530

    Add files via upload

---
+++ parenthesis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+ bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) { 
+            if (c == '(' || c == '{' || c == '[') { 
+                st.push(c); 
+            } else { 
+                if (st.empty() || 
+                    (c == ')' && st.top() != '(') ||
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false; 
+                }
+                st.pop(); 
+            }
+        }
+        return st.empty(); 
+    }
