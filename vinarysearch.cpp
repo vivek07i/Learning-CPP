8bdf3945ee0d2a0e2e417a23813dec262c3f1642
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-10-21 01:16:49 +0530

    Add files via upload

---
+++ vinarysearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+// Binary search function
+int binarySearch(const std::vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Element found, return its index
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search the right half
+        } else {
+            right = mid - 1; // Search the left half
+        }
+    }
+
+    return -1; // Element not found
+}
+
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int target = 5;
+
+    int result = binarySearch(arr, target);
+
+    if (result != -1) {
+        std::cout << "Element " << target << " found at index " << result << std::endl;
+    } else {
+        std::cout << "Element " << target << " not found in the array." << std::endl;
+    }
+
+    return 0;
+}
