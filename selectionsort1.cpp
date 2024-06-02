16e9dfe959ff517d51adf134ae3d687983d69068
Vivek <129210903+vivek07i@users.noreply.github.com>
2024-06-02 11:59:44 +0530

    Add files via upload

---
+++ selectionsort1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int min = i;
+        for (int j = i ; j <= n-1; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Original array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr, n);
+
+    cout << "\nSorted array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
