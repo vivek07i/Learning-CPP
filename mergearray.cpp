a967c69e25d9a8414ad02aa97d4642bc3ff52b28
Vivek <129210903+vivek07i@users.noreply.github.com>
2024-06-04 08:47:05 +0530

    Add files via upload

---
+++ mergearray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for the end of merged array
+        
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        
+        // If there are remaining elements in nums2, copy them
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+        
+        // No need to copy remaining elements from nums1, as they are already in place
+    }
+};
